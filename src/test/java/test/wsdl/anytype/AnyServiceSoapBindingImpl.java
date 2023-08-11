d8d5df5a8f426ea53649188d80b0e9aa615a3fe3
Sujay C Hegde <schegde@atlassian.com>
2023-08-11 16:38:41 +0530

    /CONFSRVDEV-27654: Restore lost test files

---
+++ src/test/java/test/wsdl/anytype/AnyServiceSoapBindingImpl.java
@@ -0,0 +1,14 @@
+/**
+ * AnyServiceSoapBindingImpl.java
+ * <p>
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package test.wsdl.anytype;
+
+public class AnyServiceSoapBindingImpl implements test.wsdl.anytype.AnyService {
+    public Object run() throws java.rmi.RemoteException {
+        return new String("hello");
+    }
+}
